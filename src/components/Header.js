ea9b9a8a221994eeeb80aaac7789cf44b401e85f
Minesh Tandel <minesh9174@gmail.com>
2023-02-02 01:13:02 -0500

    added functionality to outside ham drawer colaapsing

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,30 +9,42 @@ const Header = ({classname}) => {
 
     const getSection = (section) => {
         section.scrollIntoView({behavior: 'smooth'})
-        const headerMenu = document.querySelector('.header-nav-links')
-        headerMenu.className = 'header-nav-links'
+        const headerMenu = document.querySelector('.responsive-menu')
+        headerMenu.className = 'responsive-menu'
         setMenuToggle(!menuToggle)
     }
 
-    const toggleHeaderMenu = () => {
-      const headerMenu = document.querySelector('.header-nav-links')
-      headerMenu.className === 'header-nav-links' ? headerMenu.className += " responsive" : headerMenu.className = 'header-nav-links'
+    const showHeaderMenu = () => {
+      const headerMenu = document.querySelector('.responsive-menu')
+      const outsideMenu = document.querySelector('.outside-menu')
+      headerMenu.className += " responsive"
+      outsideMenu.className += " outside-responsive"
       setMenuToggle(!menuToggle)
     }
+
+    const hideHeaderMenu = () => {
+      const headerMenu = document.querySelector('.responsive-menu')
+      const outsideMenu = document.querySelector('.outside-menu')
+      headerMenu.className = 'responsive-menu'
+      outsideMenu.className = "outside-menu"
+      setMenuToggle(!menuToggle)
+    }
+
+  
 
   return (
     <div className={`header ${classname}`}>
       <div className="header-logo">MT</div>
-      <div style={{border: '1px solid yellow'}} className='responsive-menu'>
-        <div className='outside-menu'></div>
+      {menuToggle && <AiOutlineClose className='header-nav-listIcon closeButton' onClick={hideHeaderMenu}/>}
+      <div className='responsive-menu'>
+        <div className='outside-menu' onClick={hideHeaderMenu}></div>
         <ul className='header-nav-links'>
-            {menuToggle && <AiOutlineClose className='header-nav-listIcon' onClick={toggleHeaderMenu}/>}  
             <li onClick={() => getSection(document.getElementById('about'))}>About</li>
             <li onClick={() => getSection(document.getElementById('work'))}>Work</li>
             <li>Resume</li>
         </ul>
       </div>
-      {!menuToggle && <VscListSelection className='header-nav-listIcon' onClick={toggleHeaderMenu}/>}
+      {!menuToggle && <VscListSelection className='header-nav-listIcon openButton' onClick={showHeaderMenu}/>}
     </div>
   )
 }
