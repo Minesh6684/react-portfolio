aa03277daf3b669e201d24561b7e74af9c4e9ffa
Minesh Tandel <minesh9174@gmail.com>
2023-02-03 01:22:11 -0500

    URL tab icon inclusion and contact links addition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
-// import { Link } from "react-router-dom"
-import {BsArrowUpCircle} from "react-icons/bs"
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi"
 
 const Footer = () => {
   return (
     <footer className="footer p-5 text-white text-center position-relative">
+        <div className="social-links">
+          <a href="https://github.com/Minesh6684" target='_blank' rel="noreferrer"><FiGithub /></a>
+          <a href="https://www.linkedin.com/in/minesh-tandel-5a66bb1b1/" target='_blank' rel="noreferrer"><FiLinkedin /></a>
+          <a href="mailto:minesh9174@gmail.com" target='_blank' rel="noreferrer"><FiMail /></a>
+        </div>
         <p className="lead">Developed By Minesh Tandel &copy; 2022 </p>
-        <BsArrowUpCircle className="bi"/>
     </footer>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi"
 import {VscListSelection} from 'react-icons/vsc'
 import {AiOutlineClose} from 'react-icons/ai'
 import portLogo from '../img/portfolio_logo_transparent.png'
@@ -42,7 +43,12 @@ const Header = ({classname}) => {
         <ul className='header-nav-links'>
             <li onClick={() => getSection(document.getElementById('about'))}>About</li>
             <li onClick={() => getSection(document.getElementById('work'))}>Work</li>
-            <li>Resume</li>
+            <li className='header-resume'>Resume</li>
+            <div className="social-links-header">
+              <a href="https://github.com/Minesh6684" target='_blank' rel="noreferrer"><FiGithub /></a>
+              <a href="https://www.linkedin.com/in/minesh-tandel-5a66bb1b1/" target='_blank' rel="noreferrer"><FiLinkedin /></a>
+              <a href="mailto:minesh9174@gmail.com" target='_blank' rel="noreferrer"><FiMail /></a>
+            </div>
         </ul>
       </div>
       {!menuToggle && <VscListSelection className='header-nav-listIcon openButton' onClick={showHeaderMenu}/>}
