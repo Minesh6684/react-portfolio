1a83075ff459807e99ef786e7959fe516c2e5bac
Minesh Tandel <minesh9174@gmail.com>
2023-06-17 13:05:57 -0400

    Updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,101 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi"
-import {VscListSelection} from 'react-icons/vsc'
-import {AiOutlineClose} from 'react-icons/ai'
-import portLogo from '../img/portfolio_logo_transparent.png'
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
+import { VscListSelection } from "react-icons/vsc";
+import { AiOutlineClose } from "react-icons/ai";
+import portLogo from "../img/portfolio_logo_transparent.png";
 
-const Header = ({classname}) => {
+const Header = ({ classname }) => {
+  const [menuToggle, setMenuToggle] = useState(false);
 
-    const [menuToggle, setMenuToggle] = useState(false)
+  const getSection = (section) => {
+    hideHeaderMenu();
+    section.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const getSection = (section) => {
-        hideHeaderMenu()
-        section.scrollIntoView({behavior: 'smooth'})
-    }
+  const showHeaderMenu = () => {
+    const headerMenu = document.querySelector(".responsive-menu");
+    const outsideMenu = document.querySelector(".outside-menu");
+    const body = document.querySelector("body");
+    body.className = "prevent-body-scroll";
+    headerMenu.className += " responsive";
+    outsideMenu.className += " outside-responsive";
+    setMenuToggle(!menuToggle);
+  };
 
-    const showHeaderMenu = () => {
-      const headerMenu = document.querySelector('.responsive-menu')
-      const outsideMenu = document.querySelector('.outside-menu')
-      const body = document.querySelector('body')
-      body.className = 'prevent-body-scroll'
-      headerMenu.className += " responsive"
-      outsideMenu.className += " outside-responsive"
-      setMenuToggle(!menuToggle)
-    }
-
-    const hideHeaderMenu = () => {
-      const headerMenu = document.querySelector('.responsive-menu')
-      const outsideMenu = document.querySelector('.outside-menu')
-      const body = document.querySelector('body')
-      body.className = ''
-      headerMenu.className = 'responsive-menu'
-      outsideMenu.className = "outside-menu"
-      setMenuToggle(!menuToggle)
-    }
-
-  
+  const hideHeaderMenu = () => {
+    const headerMenu = document.querySelector(".responsive-menu");
+    const outsideMenu = document.querySelector(".outside-menu");
+    const body = document.querySelector("body");
+    body.className = "";
+    headerMenu.className = "responsive-menu";
+    outsideMenu.className = "outside-menu";
+    setMenuToggle(!menuToggle);
+  };
 
   return (
     <div className={`header ${classname}`}>
-      <div className="header-logo"><img src={portLogo} alt="" className='portfolio-logo'/></div>
-      {menuToggle && <AiOutlineClose className='header-nav-listIcon closeButton' onClick={hideHeaderMenu}/>}
-      <div className='responsive-menu'>
-        <div className='outside-menu' onClick={hideHeaderMenu}></div>
-        <ul className='header-nav-links'>
-            <li onClick={() => getSection(document.getElementById('about'))}>About</li>
-            <li onClick={() => getSection(document.getElementById('work'))}>Work</li>
-            <li ><a className='header-resume' href='https://drive.google.com/file/d/1Eeq1oKWnQBmVVcZknleIn2PVvPc3Jmuu/view' target="_blank" rel="noreferrer">Resume</a></li>
-            <div className="social-links-header">
-              <a href="https://github.com/Minesh6684" target='_blank' rel="noreferrer"><FiGithub /></a>
-              <a href="https://www.linkedin.com/in/minesh-tandel-5a66bb1b1/" target='_blank' rel="noreferrer"><FiLinkedin /></a>
-              <a href="mailto:minesh9174@gmail.com" target='_blank' rel="noreferrer"><FiMail /></a>
-            </div>
+      <div className="header-logo">
+        <img src={portLogo} alt="" className="portfolio-logo" />
+      </div>
+      {menuToggle && (
+        <AiOutlineClose
+          className="header-nav-listIcon closeButton"
+          onClick={hideHeaderMenu}
+        />
+      )}
+      <div className="responsive-menu">
+        <div className="outside-menu" onClick={hideHeaderMenu}></div>
+        <ul className="header-nav-links">
+          <li onClick={() => getSection(document.getElementById("about"))}>
+            About
+          </li>
+          <li onClick={() => getSection(document.getElementById("work"))}>
+            Work
+          </li>
+          <li>
+            <a
+              className="header-resume"
+              href="https://drive.google.com/file/d/1-Bvn__1azoFwDSrFzrUmWxXLrvJpBt3w/view?usp=drive_link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+          </li>
+          <div className="social-links-header">
+            <a
+              href="https://github.com/Minesh6684"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/minesh-tandel-5a66bb1b1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiLinkedin />
+            </a>
+            <a
+              href="mailto:minesh9174@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiMail />
+            </a>
+          </div>
         </ul>
       </div>
-      {!menuToggle && <VscListSelection className='header-nav-listIcon openButton' onClick={showHeaderMenu}/>}
+      {!menuToggle && (
+        <VscListSelection
+          className="header-nav-listIcon openButton"
+          onClick={showHeaderMenu}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,27 @@
 const Intro = () => {
   return (
     <div className="intro">
-      <p className="intro-name-line" style={{color: "#63fedb"}}>Hi, my name is</p>
+      <p className="intro-name-line" style={{ color: "#63fedb" }}>
+        Hi, my name is
+      </p>
       <div className="intro-name-designation">
         <h1 className="intro-name">Minesh Tandel.</h1>
         <h2 className="intro-designation">I develop things for the web.</h2>
       </div>
-      <p className="intro-current-role">I am an Internet Programming student at TAV College, Montreal. Currently I'am focused towards learning Three.js.</p>
-      <a className="intro-cv" href="https://drive.google.com/file/d/1Eeq1oKWnQBmVVcZknleIn2PVvPc3Jmuu/view" target="_blank" rel="noreferrer">Download CV</a>
+      <p className="intro-current-role">
+        I am an Internet Programming student at TAV College, Montreal. Currently
+        I'am focused towards learning Three.js.
+      </p>
+      <a
+        className="intro-cv"
+        href="https://drive.google.com/file/d/1-Bvn__1azoFwDSrFzrUmWxXLrvJpBt3w/view?usp=drive_link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Download CV
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
